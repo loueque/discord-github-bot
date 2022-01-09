0a933405a9169402fa0e1590a564dd8b11f94271
loueque <louequerodab@gmail.com>
2022-01-08 18:56:50 -0800

    wikipedia

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import discord
+
 
 class Utility(commands.Cog):
     def __init__(self, bot):
@@ -13,9 +13,31 @@ class Utility(commands.Cog):
     async def conventional(self, ctx):
         await ctx.send('https://www.conventionalcommits.org/en/v1.0.0/')
 
-    @commands.codeblock()
-    async def codeblock(self, ctx, *, lang, code):
-        await ctx.send(f'```{lang}\n{code}```')
+    @commands.command()
+    async def nohello(self, ctx):
+        await ctx.send('https://nohello.net/')
+
+    @commands.command()
+    async def wikipedia(self, ctx):
+        await ctx.send('https://en.wikipedia.org/wiki/Main_Page')
+
+    @commands.command()
+    async def xkcd(self, ctx, arguments = None):
+        if not arguments:
+            await ctx.send('Please provide arguments e.g g!xkcd <comic_number>')
+        else:
+            if requests.get('https://xkcd.com/' + arguments + '/info.0.json').status_code == 200:
+                xkcd_result = requests.get('https://xkcd.com/' + arguments + '/info.0.json')
+                xkcd_result_json = xkcd_result.json()
+                xkcd_embed = discord.Embed(
+                    title="xkcd: " + xkcd_result_json['safe_title'],
+                    url='https://xkcd.com/' + str(arguments) + '/',
+                    type='image',
+                    description=xkcd_result_json['alt']
+                    )
+                xkcd_embed.set_image(url=xkcd_result_json['img'])
+                xkcd_embed.set_footer(text='xkcd #' + str(arguments))
+                await ctx.send(embed=xkcd_embed)
 
 def setup(bot):
     bot.add_cog(Utility(bot))
